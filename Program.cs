57dae92ccea7e7b3fd39dc14da302679b58a8bb3
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2022-06-18 13:59:06 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                numbers.Add(i);
+            }
+
+            List<int> oddNumbers = numbers.FindAll(x => x % 2 != 0);
+
+            oddNumbers.ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
